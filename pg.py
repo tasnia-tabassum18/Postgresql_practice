29cdfb616b99c03741fa273ea96a753836679a27
tasnia-tabassum18 <1910040@iub.edu.bd>
2023-06-01 10:46:26 +0600

    insert data into table code added

--- pg.py
+++ pg.py
@@ -54,3 +54,59 @@ def create_table():
     conn.close()
 
 create_table()
+
+#creating table in postgres db
+def create_table():
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="employee",
+        user="postgres",
+        password="oishee"
+    )
+
+    cursor = conn.cursor()
+
+    table_name = "employee_t"
+    table_schema = """
+                    CREATE TABLE IF NOT EXISTS employee_t (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255),
+                        designation VARCHAR(60),
+                        salary VARCHAR(100)
+                    )
+    """.format(table=table_name)
+
+    cursor.execute(table_schema)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+create_table()
+
+#inserting data 
+
+
+def insert_data():
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="employee",
+        user="postgres",
+        password="oishee"
+    )
+
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO employee_t (id, name, designation,salary) VALUES (%s, %s, %s, %s)"
+    values = ("1", "Aelin", "Software Engineer", "50000")
+
+    cursor.execute(insert_query, values)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+insert_data()
